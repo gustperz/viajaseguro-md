43cadbf3bb62b8017db5b2a9b38ecc41f034fb96
Gustavo Pérez Machado <gustperz@gmail.com>
2016-09-12 14:03:42 -0500

    jwt injector

--- src/app/auth/auth.module.js
+++ src/app/auth/auth.module.js
@@ -5,7 +5,7 @@
     angular
         .module('app.auth', [
             'permission', 'permission.ui', 'angular-jwt', 'app.constansts',
-            'auth.login'
+            'app.auth.login'
         ])
         .config(config);
 
