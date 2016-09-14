28ac72acf17e483d4b64abd3c1c3f83ccb916a47
Gustavo Pérez Machado <gustperz@gmail.com>
2016-09-13 20:41:18 -0500

    restangular config extract data

--- src/app/config/restangular.config.js
+++ src/app/config/restangular.config.js
@@ -10,5 +10,17 @@
     function routeConfig(RestangularProvider, api)
     {
         RestangularProvider.setBaseUrl(api);
+        RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+            var extractedData;
+            // .. to look for getList operations
+            extractedData = data;
+            if (data.data) {
+                extractedData = data.data;
+                delete data.data;
+                extractedData.metadata = data;
+                extractedData.metadata.status = response.status;
+            }
+            return extractedData;
+        });
     }
 })();
