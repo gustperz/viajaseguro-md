25ebc8ef0e24fc3dc9fa3c9518bb703c625ffa70
Jose Miguel Soto Acosta <jomisoac@gmail.com>
2016-09-29 09:01:08 -0500

    1. solicitudes added to the queue. 2. rejection of solicitudes in socket.

--- src/app/main/centrales/despacho/lista_solicitudes/lista_solicitudes.controller.js
+++ src/app/main/centrales/despacho/lista_solicitudes/lista_solicitudes.controller.js
@@ -8,14 +8,57 @@
         .module('app.centrales.despacho')
         .controller('ListaSolicitudesController', controller);
 
-    function controller(SocketcSailsService, authService, $sails) {
+    function controller(SocketcSailsService, authService, $sails, $mdMenu) {
         SocketcSailsService.suscribe(authService.getCurrentUser().central.id);
         var vmsolicitudes = this;
+        vmsolicitudes.solicitud = {};
         vmsolicitudes.solicitudes = JSON.parse(sessionStorage.getItem('solicitudes')) || [];
+
+        // metodos
+        vmsolicitudes.cancelNesSolicitud = cancelNewSolicitud;
+        vmsolicitudes.saveNewSolicitud = saveNewSolicitud;
+        vmsolicitudes.rejectSolicitud = rejectSolicitud;
 
         $sails.on('newSolicitud', function (response) {
             vmsolicitudes.solicitudes.push(SocketcSailsService.newSolicitud(response));
         });
 
+        function cancelNewSolicitud() {
+            vmsolicitudes.solicitud = {};
+            $mdMenu.hide();
+        }
+
+        function saveNewSolicitud() {
+            vmsolicitudes.solicitudes.push(vmsolicitudes.solicitud);
+            sessionStorage.setItem('solicitudes', JSON.stringify(vmsolicitudes.solicitudes));
+            $mdMenu.hide();
+        }
+
+        function rejectSolicitud($index) {
+            var solicitud = vmsolicitudes.solicitudes[$index];
+            if(solicitud.id){
+                $sails.request({
+                    method: 'post',
+                    url: '/centrales/reject_solicitud' ,
+                    data: solicitud,
+                    headers: {
+                        'Authorization': 'Bearer ' + sessionStorage.getItem('jwt')
+                    }
+                }, function (response) {
+                    console.log(response)
+                    if (response.code == 'OK'){
+                        vmsolicitudes.solicitudes.splice($index, 1);
+                        sessionStorage.setItem('solicitudes', JSON.stringify(vmsolicitudes.solicitudes));
+                    }else{
+                        console.log('fallo')
+                    }
+                });
+            }else{
+                vmsolicitudes.solicitudes.splice($index, 1);
+                sessionStorage.setItem('solicitudes', JSON.stringify(vmsolicitudes.solicitudes));
+            }
+        }
+
     }
 })();
+//
