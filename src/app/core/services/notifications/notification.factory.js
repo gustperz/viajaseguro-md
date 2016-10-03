c419720486e78722d53938b2f350cd153837c828
Gustavo Pérez Machado <gustperz@gmail.com>
2016-10-03 13:57:46 -0500

    nofication factory refactor

--- src/app/core/services/notifications/notification.factory.js
+++ src/app/core/services/notifications/notification.factory.js
@@ -6,60 +6,20 @@
 
     angular
         .module('app.core')
-        .factory('NotifitationFactory', NotifitationFactory);
+        .factory('Notify', NotifitationFactory);
 
     /* @ngInject */
-    function NotifitationFactory() {
-        return {
-            'requestPermission': requestPermission,
-            'onSolicitudRecived': onSolicitudRecived,
-            'onNotificationRecived': onNotificationRecived
-        };
-
-        function requestPermission() {
-            if (Notification.permission !== "granted") {
-                Notification.requestPermission();
-            }
-        }
-
-        function onSolicitudRecived(data) {
+    function NotifitationFactory(Toast) {
+        return function (data) {
             var options = {
                 body: data.message
             };
-            var notificacion = new Notification('Nueva notificación de: ' + data.tipo, options);
-            if (!isNotificationSupported()) {
-                logg("Tu navegador no soporta Notificaciones. Por favor, utiliza una versión Reciente del Navegador Google Chrome o Safari.");
-                return;
+            var notificacion = new Notification(data.tittle, options);
+            if ('Notification' in window) {
+                setTimeout(notificacion.close.bind(notificacion), 10000);
+            } else {
+                Toast(data.message);
             }
-            // Si el Navegador soporta las Notificaciones HTML 5, entonces que proceda a Notificar
-            setTimeout(notificacion.close.bind(notificacion), 10000);
-        }
-
-
-
-         function onNotificationRecived(data) {
-            var options = {
-                body: data.message
-            };
-            if (!isNotificationSupported()) {
-                logg("Tu navegador no soporta Notificaciones. Por favor, utiliza una versión Reciente del Navegador Google Chrome o Safari.");
-                return;
-
-            }
-            // Si el Navegador soporta las Notificaciones HTML 5, entonces que proceda a Notificar
-            var notificacion = new Notification('Nueva: ' + data.tipo, options);
-            setTimeout(notificacion.close.bind(notificacion), 10000);
-        }
-
-
-        // Luego del Permiso del Sistema, le indicamos que nos Muestre la Notificación
-        function isNotificationSupported() {
-            return ("Notification" in window);
-        }
-
-        // Mostramos el Mensaje de la Notificación
-        function logg(mensaje) {
-            notificador.innerHTML += "<p>" + mensaje + "</p>";
         }
     }
 })();
