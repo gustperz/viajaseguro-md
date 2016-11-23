276a4e98453c7b746f96b0b4de2e7f8dbaaaaf00
Gustavo Pérez Machado <gustperz@gmail.com>
2016-11-23 00:37:06 -0500

    despachos data

--- src/app/main/centrales/despacho/despacho.controller.js
+++ src/app/main/centrales/despacho/despacho.controller.js
@@ -43,9 +43,19 @@
         }
 
         function despachar() {
-            if(!Despacho.contratante || !(Despacho.sa[Despacho.conductor.id] && Despacho.sa[Despacho.conductor.id].length )) {
+            if(!Despacho.conductor.vehiculo){
+                return Toast('Este conductor no tiene asignado un vehiculo, no es posible realizar el despacho');                
+            }
+            if(!(Despacho.sa[Despacho.conductor.id] && Despacho.sa[Despacho.conductor.id].length )) {
+                return Toast('No puedes realizar un despacho sin pasajeros');
+            }
+            if(Despacho.conductor.vehiculo.modalidad == 'especial' && !Despacho.contratante) {
                 return Toast('Es necesario establecer un pasajero como contratante para realizar el despacho');
             }
+            if(!Despacho.valor_viaje){
+                return Toast('Y el valor del viaje?');
+            }
+            
             var pasajeros = [];
             Despacho.sa[Despacho.conductor.id].forEach(function (solicitud) {
                 pasajeros = pasajeros.concat(solicitud.pasajeros);
@@ -59,8 +69,8 @@
                 valor: Despacho.valor_viaje,
                 conductor: Despacho.conductor.id,
                 vehiculo: typeof Despacho.conductor.vehiculo == 'object' ? Despacho.conductor.vehiculo.id : Despacho.conductor.vehiculo,
-                contratante_identificacion: Despacho.contratante.identificacion,
-                contratante_nombre: Despacho.contratante.nombre,
+                contratante_identificacion: Despacho.contratante ? Despacho.contratante.identificacion : null,
+                contratante_nombre: Despacho.contratante ? Despacho.contratante.nombre : null,
                 pasajeros: pasajeros
             }
 
