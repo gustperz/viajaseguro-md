31630fb1e1a96dcbace6d915e74fd41da136ae92
Gustavo Pérez Machado <gustperz@gmail.com>
2017-01-12 13:19:00 -0500

    toast text size

--- src/app/core/services/toast.factory.js
+++ src/app/core/services/toast.factory.js
@@ -14,14 +14,22 @@
         return function (text, position, delay, parent) {
             $mdToast.show({
                 template: '' +
-                '<md-toast id="language-message" layout="column" layout-align="center start">' +
-                '<div class="md-toast-content">' +
-                text +
+                '<md-toast layout="column" layout-align="center start">' +
+                '<div class="md-toast-content" style="font-size: 20px">' +
+                    text +
+                    '<md-button class="md-icon-button" aria-label="cerrar" ng-click="closeToast()" style="margin-right: -22px">'+
+                        '<md-icon md-font-icon="icon-close" style="color: grey; font-size: 12px"></md-icon>'+
+                    '</md-button>'+
                 '</div>' +
                 '</md-toast>',
-                hideDelay: delay || 4000,
+                hideDelay: delay || 7000,
                 position: position || 'top right',
-                parent: angular.element(document.body)
+                parent: angular.element(document.body),
+                controller: function ($scope, $mdToast) {
+                    $scope.closeToast = function() {
+                        $mdToast.hide();
+                    }
+                }
             });
         }
     }
